b7abaffebd4f87ff6ea1d7a729dfe81b19913b7c
SakYyPheonix <charlybourgouin@gmail.com>
2020-01-23 11:13:12 +0100

    commit initial

---
+++ 01_Introductions/01_1_PresentationJAVA/Exercice1/src/main/java/Exercice1/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercice1;
+
+/**
+ *
+ * @author cbourgouin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i;
+        for(i=0; i<12; i++){
+            System.out.println("7 x "+i+" = "+7*i);
+        }
+    }
+    
+}
